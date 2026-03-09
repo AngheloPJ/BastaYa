7be57110b9f3b7437d81f885e918d61f8c5cb3bd
Ikerby <novoliva@gmail.com>
2026-03-09 16:53:49 +0100

    Cambios que me suben el colesterol –🐈‍⬛

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,20 @@
-"use strict";
-let connexio = new WebSocket('ws://localhost:8180');
+const connexio = new WebSocket('ws://localhost:8180');
 const playButton = document.getElementById('playButton');
 const nicknameInput = document.getElementById('playerName');
 playButton.addEventListener('click', play);
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+    if (document.getElementById('page-' + pageId) !== null) {
+        document.getElementById('page-' + pageId).classList.add('active');
+    }
+}
 function play() {
-    connexio.send('connection');
     if (nicknameInput.value.trim() !== '') {
         let jsonData = JSON.stringify({ type: "set_nickname", nickname: nicknameInput.value });
         connexio.send(jsonData);
     }
-    window.location.href = 'lobby.html';
+    showPage("lobby");
 }
-//# sourceMappingURL=script.js.map
+export function getConnexio() {
+    return connexio;
+}
--- srcPublic/script.ts
+++ srcPublic/script.ts
@@ -1,14 +1,25 @@
-let connexio = new WebSocket('ws://localhost:8180');
+const connexio = new WebSocket('ws://localhost:8180');
 
 const playButton = document.getElementById('playButton') as HTMLButtonElement;
 const nicknameInput = document.getElementById('playerName') as HTMLInputElement;
 
 playButton.addEventListener('click', play);
 
+function showPage(pageId : string) {
+    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+    if (document.getElementById('page-' + pageId) !== null) {
+        document.getElementById('page-' + pageId)!.classList.add('active');
+    }
+}
+
 function play() {
     if (nicknameInput.value.trim() !== '') {
         let jsonData = JSON.stringify({ type: "set_nickname", nickname: nicknameInput.value });
         connexio.send(jsonData);
     }
-    window.location.href = 'lobby.html';
+    showPage("lobby");
+}
+
+export function getConnexio() {
+    return connexio;
 }

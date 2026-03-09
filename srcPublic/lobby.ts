b6a6ae5c1b68385499392b0dce5042ecb041caf6
Ikerby <novoliva@gmail.com>
2026-03-09 16:17:15 +0100

    Lobby features

---
+++ srcPublic/lobby.ts
@@ -0,0 +1,22 @@
+init();
+
+const playerList = document.getElementById("playerList") as HTMLUListElement;
+const chatMessages = document.getElementById("chatMessages") as HTMLUListElement;
+const chatInput = document.getElementById("chatInput") as HTMLInputElement;
+const sendButton = document.getElementById("sendChatButton") as HTMLButtonElement;
+const startGameButton = document.getElementById("startGameButton") as HTMLButtonElement;
+
+sendButton.addEventListener("click", sendChatMessage);
+startGameButton.addEventListener("click", startGame);
+
+function init() {
+
+}
+
+function sendChatMessage() {
+
+}
+
+function startGame() {
+    
+}

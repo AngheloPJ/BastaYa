f52f859160d6aa21ad89c9b591ec04da90f65a4c
Ikerby <novoliva@gmail.com>
2026-03-09 15:57:09 +0100

    Iniciació del frontend

---
+++ srcPublic/script.ts
@@ -0,0 +1,15 @@
+let connexio = new WebSocket('ws://localhost:8180');
+
+const playButton = document.getElementById('playButton') as HTMLButtonElement;
+const nicknameInput = document.getElementById('playerName') as HTMLInputElement;
+
+playButton.addEventListener('click', play);
+
+function play() {
+    connexio.send('connection');
+    if (nicknameInput.value.trim() !== '') {
+        let jsonData = JSON.stringify({ type: "set_nickname", nickname: nicknameInput.value });
+        connexio.send(jsonData);
+    }
+    window.location.href = 'lobby.html';
+}
